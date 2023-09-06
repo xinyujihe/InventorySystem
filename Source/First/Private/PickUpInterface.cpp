f7860ff6bfa1763a95635836647938f438c449c5
心予芰荷 <1154691205@qq.com>
2023-09-06 22:47:36 +0800

    Revert "2023/9/6重做了整体的背包UI和Slot，使其更像塞尔达"
    This reverts commit a0e7fe8184432e53f2d51a66a40016f495a70fe3.

--- Source/First/Private/PickUpInterface.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "PickUpInterface.h"
-
-// Add default functionality here for any IPickUpInterface functions that are not pure virtual.
-
